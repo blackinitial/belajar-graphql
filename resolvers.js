c1131654e0207ae681de8941e54208300632ee22
blackinitial <alhanif.blackinitial@gmail.com>
2021-09-01 21:10:54 +0700

    create mutation

--- resolvers.js
+++ resolvers.js
@@ -3,5 +3,13 @@ const BookModel = require('./models/Book');
 module.exports = {
   Query: {
     getAllBooks: async () => await BookModel.find({})
+  },
+
+  Mutation: {
+    createBook: async (_, args) => {
+      const book = new BookModel(args);
+      await book.save();
+      return book;
+    }
   }
 }
--- schema.js
+++ schema.js
@@ -13,6 +13,16 @@ const typeDefs = gql`
   type Query {
     getAllBooks: [Book]!
   }
+
+  type Mutation {
+    createBook(
+      title: String!,
+      author: String!,
+      description: String,
+      release_year: Int!,
+      genre: String!
+    ): Book!
+  }
 `;
 
 module.exports = typeDefs;
